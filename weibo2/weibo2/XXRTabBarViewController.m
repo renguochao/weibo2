ef6d4b9e3276bd38ef85e7b87d454e32b4029bc9
Guochao Ren <314017427@qq.com>
2015-08-11 12:34:38 +0800

    重构页面控制器定义代码

--- weibo2/weibo2/XXRTabBarViewController.m
+++ weibo2/weibo2/XXRTabBarViewController.m
@@ -26,45 +26,45 @@
     // 1.首页
     XXRHomeViewController *home = [[XXRHomeViewController alloc] init];
     home.view.backgroundColor = [UIColor redColor];
-    home.title = @"首页"; // 设置title属性等于下方两个设置
-//    home.tabBarItem.title = @"首页";
-//    home.navigationItem.title = @"首页";
-    home.tabBarItem.image = [UIImage imageNamed:@"tabbar_home"];
-    home.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_home_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal]; //告诉iOS不要渲染图片
-    UINavigationController *homeNav = [[UINavigationController alloc] initWithRootViewController:home];
-    [self addChildViewController:homeNav];
+    [self setupChildViewController:home title:@"首页" imageName:@"tabbar_home" selectedImageName:@"tabbar_home_selected"];
     
     // 2.消息
     XXRMessageViewController *message = [[XXRMessageViewController alloc] init];
     message.view.backgroundColor = [UIColor blueColor];
-    message.title = @"消息";
-//    message.tabBarItem.title = @"消息";
-    message.tabBarItem.image = [UIImage imageNamed:@"tabbar_message_center"];
-    message.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_message_center_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    UINavigationController *messageNav = [[UINavigationController alloc] initWithRootViewController:message];
-    [self addChildViewController:messageNav];
+    [self setupChildViewController:message title:@"消息" imageName:@"tabbar_message_center" selectedImageName:@"tabbar_message_center_selected"];
     
     // 3.广场
     XXRDiscoverViewController *discover = [[XXRDiscoverViewController alloc] init];
     discover.view.backgroundColor = [UIColor blackColor];
-    discover.title = @"广场";
-//    discover.tabBarItem.title = @"广场";
-    discover.tabBarItem.image = [UIImage imageNamed:@"tabbar_discover"];
-    discover.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_discover_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    UINavigationController *discoverNav = [[UINavigationController alloc] initWithRootViewController:discover];
-    [self addChildViewController:discoverNav];
+    [self setupChildViewController:discover title:@"广场" imageName:@"tabbar_discover" selectedImageName:@"tabbar_discover_selected"];
     
     // 4.我
     XXRMeViewController *me = [[XXRMeViewController alloc] init];
     me.view.backgroundColor = [UIColor grayColor];
-    me.title = @"我";
-//    me.tabBarItem.title = @"我";
-    me.tabBarItem.image = [UIImage imageNamed:@"tabbar_profile"];
-    me.tabBarItem.selectedImage = [[UIImage imageNamed:@"tabbar_profile_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    UINavigationController *meNav = [[UINavigationController alloc] initWithRootViewController:me];
-    [self addChildViewController:meNav];
+    [self setupChildViewController:me title:@"我" imageName:@"tabbar_profile" selectedImageName:@"tabbar_profile_selected"];
 }
 
+/**
+ *  初始化一个子控制器
+ *
+ *  @param childVc           需要初始化的子控制器
+ *  @param title             标题
+ *  @param imageName         图标名
+ *  @param selectedImageName 选中图标名
+ */
+- (void)setupChildViewController:(UIViewController *)childVc title:(NSString *)title imageName:(NSString *)imageName selectedImageName:(NSString *)selectedImageName {
+    
+    // 1.设置控制器的属性
+    childVc.title = title;
+    //    home.tabBarItem.title = @"首页";
+    //    home.navigationItem.title = @"首页";
+    childVc.tabBarItem.image = [UIImage imageNamed:imageName];
+    childVc.tabBarItem.selectedImage = [[UIImage imageNamed:selectedImageName ] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    // 2.包装导航控制器
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:childVc];
+    [self addChildViewController:nav];
+}
 
 
 @end
