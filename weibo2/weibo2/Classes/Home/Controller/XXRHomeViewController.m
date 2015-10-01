1e58553f8fa70c920c4019f03faf9d1634fa4542
Guochao Ren <314017427@qq.com>
2015-10-01 15:03:47 +0800

    实现首页微博数据拉取、展示功能

--- weibo2/weibo2/Classes/Home/Controller/XXRHomeViewController.m
+++ weibo2/weibo2/Classes/Home/Controller/XXRHomeViewController.m
@@ -8,8 +8,21 @@
 
 #import "XXRHomeViewController.h"
 
-@interface XXRHomeViewController ()
+#import "Common.h"
+#import "UIBarButtonItem+XXR.h"
+#import "UIImage+XXR.h"
+#import "XXRTitleButton.h"
+#import "AFNetworking.h"
+#import "XXRAccountTool.h"
+#import "XXRAccount.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+#import "XXRStatus.h"
 
+#define kXXRTitleButtonDownTag 0
+#define kXXRTitleButtonUpTag -1
+
+@interface XXRHomeViewController ()
+@property (nonatomic, strong) NSArray *statuses;
 @end
 
 @implementation XXRHomeViewController
@@ -17,84 +30,117 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
+    // 设置导航栏
+    [self setupNavigationBar];
     
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    // 加载微博数据
+    [self loadWeiboStatusData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadWeiboStatusData {
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+    
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"access_token"] = [XXRAccountTool account].access_token;
+    
+    [mgr GET:@"https://api.weibo.com/2/statuses/home_timeline.json" parameters:params success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        // 取出所有的微博数据
+        NSArray *dictArray = responseObject[@"statuses"];
+        
+        // 将字典数据转换成模型数据
+        NSMutableArray *statusArray = [NSMutableArray array];
+        for (NSDictionary *dict in dictArray) {
+            // 创建模型
+            XXRStatus *status = [XXRStatus statusWithDict:dict];
+            
+            // 添加模型
+            [statusArray addObject:status];
+        }
+        self.statuses = statusArray;
+        
+        [self.tableView reloadData];
+        XXRLog(@"----%@:", responseObject);
+    } failure:^(AFHTTPRequestOperation * _Nonnull operation, NSError * _Nonnull error) {
+        
+    }];
+    
+}
+
+- (void)setupNavigationBar {
+    // 左边按钮
+    self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithIcon:@"navigationbar_friendsearch_os7" highIcon:@"navigationbar_friendsearch_highlighted_os7" target:self action:@selector(findFriend)];
+    
+    // 右边按钮
+    self.navigationItem.rightBarButtonItem = [UIBarButtonItem itemWithIcon:@"navigationbar_pop_os7" highIcon:@"navigationbar_pop_highlighted_os7" target:self action:@selector(pop)];
+    
+    // 中间按钮
+    XXRTitleButton *titleButton = [[XXRTitleButton alloc] init];
+    
+    // 图标
+    [titleButton setImage:[UIImage imageNamed:@"navigationbar_arrow_down_os7"] forState:UIControlStateNormal];
+    
+    // 文字
+    [titleButton setTitle:@"拉风的小囊" forState:UIControlStateNormal];
+    titleButton.frame = CGRectMake(0, 0, 130, 35);
+    titleButton.tag = kXXRTitleButtonDownTag;
+    [titleButton addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.titleView = titleButton;
+}
+
+- (void)titleClick:(XXRTitleButton *)titleButton {
+    if (titleButton.tag == kXXRTitleButtonUpTag) {
+        [titleButton setImage:[UIImage imageNamed:@"navigationbar_arrow_down_os7"] forState:UIControlStateNormal];
+        titleButton.tag = kXXRTitleButtonDownTag;
+    } else {
+        [titleButton setImage:[UIImage imageNamed:@"navigationbar_arrow_up_os7"] forState:UIControlStateNormal];
+        titleButton.tag = kXXRTitleButtonUpTag;
+    }
+}
+
+- (void)pop {
+    XXRLog(@"pop");
+}
+
+- (void)findFriend {
+    XXRLog(@"findFriend");
 }
 
 #pragma mark - Table view data source
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-#warning Potentially incomplete method implementation.
-    // Return the number of sections.
-    return 0;
-}
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-#warning Incomplete method implementation.
-    // Return the number of rows in the section.
-    return 0;
+    return self.statuses.count;
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
     
-    // Configure the cell...
+    // 1.创建cell
+    static NSString *ID = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:ID];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:ID];
+    }
     
+    // 2.设置cell的数据
+    // 微博内容
+    XXRStatus *status = self.statuses[indexPath.row];
+    cell.textLabel.text = status.text;
+    
+    // 微博作者
+    XXRUser *user = status.user;
+    cell.detailTextLabel.text = user.name;
+    
+    // 作者头像
+    NSString *iconUrl = user.profile_image_url;
+    [cell.imageView sd_setImageWithURL:[NSURL URLWithString:iconUrl] placeholderImage:[UIImage imageNamed:@"find_people"] options:SDWebImageRetryFailed | SDWebImageLowPriority];
+//    NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:iconUrl]];
+//    cell.imageView.image = [UIImage imageWithData:imageData];
     return cell;
 }
-*/
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view.backgroundColor = [UIColor blackColor];
+    [self.navigationController pushViewController:vc animated:YES];
 }
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
