553313482c42bc23d0b3f543cf9aba818316c257
Guochao Ren <314017427@qq.com>
2015-10-01 15:05:19 +0800

    发现页面导航栏添加SearchBar

--- weibo2/weibo2/Classes/Discover/Controller/XXRDiscoverViewController.m
+++ weibo2/weibo2/Classes/Discover/Controller/XXRDiscoverViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "XXRDiscoverViewController.h"
+#import "UIImage+XXR.h"
+#import "XXRSearchBar.h"
 
 @interface XXRDiscoverViewController ()
 
@@ -17,16 +19,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    // Uncomment the following line to preserve selection between presentations.
-    // self.clearsSelectionOnViewWillAppear = NO;
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    XXRSearchBar *searchBar = [XXRSearchBar searchBar];
+    // 尺寸
+    searchBar.frame = CGRectMake(0, 0, 320, 30);
+    // 设置中间的标题内容
+    self.navigationItem.titleView = searchBar;
 }
 
 #pragma mark - Table view data source
