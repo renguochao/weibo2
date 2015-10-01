93833c9774e64de048715d82dd39e3170f3e0ef9
Guochao Ren <314017427@qq.com>
2015-10-01 15:14:59 +0800

    在Common.h中添加宏 * 根据RGB颜色获取UIColor * 添加自定义Log

--- weibo2/weibo2/Classes/Other/Common.h
+++ weibo2/weibo2/Classes/Other/Common.h
@@ -11,4 +11,16 @@
 
 // 1.判断是否为iOS7
 #define iOS7 ([[UIDevice currentDevice].systemVersion doubleValue] >= 7.0)
+
+// 2.获得RGB颜色
+#define XXRColor(r, g, b) [UIColor colorWithRed:(r)/255.0 green:(g)/255.0 blue:(b)/255.0 alpha:1.0]
+
+// 3.自定义Log
+#ifdef DEBUG
+#define XXRLog(...) NSLog(__VA_ARGS__)
+#else
+#define XXRLog(...)
 #endif
+
+#endif
+
