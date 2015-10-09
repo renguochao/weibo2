ec1671d5e6874b54c84ea2a0979e753be5067f19
Guochao Ren <314017427@qq.com>
2015-10-09 15:20:00 +0800

    提交Podfile

---
+++ weibo2/weibo2/Classes/Other/XXRAccountTool.m
@@ -0,0 +1,13 @@
+//
+//  XXRAccountTool.m
+//  weibo2
+//
+//  Created by rgc on 15/9/30.
+//  Copyright © 2015年 rgc. All rights reserved.
+//
+
+#import "XXRAccountTool.h"
+
+@implementation XXRAccountTool
+
+@end
