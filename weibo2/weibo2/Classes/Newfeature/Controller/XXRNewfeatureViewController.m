f05a4b58ca08348a0ab98579f10d8bceae4cc6a0
Guochao Ren <314017427@qq.com>
2015-10-09 15:04:24 +0800

    添加新特性页面

---
+++ weibo2/weibo2/Classes/Newfeature/Controller/XXRNewfeatureViewController.m
@@ -0,0 +1,38 @@
+//
+//  XXRNewfeatureViewController.m
+//  weibo2
+//
+//  Created by rgc on 15/9/30.
+//  Copyright © 2015年 rgc. All rights reserved.
+//
+
+#import "XXRNewfeatureViewController.h"
+
+@interface XXRNewfeatureViewController ()
+
+@end
+
+@implementation XXRNewfeatureViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
