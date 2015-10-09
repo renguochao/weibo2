15989ad1512f024752bd03a16b3ae2345ddd0c62
Guochao Ren <314017427@qq.com>
2015-10-09 14:56:45 +0800

    取出微博cell周围的白色边框

--- weibo2/weibo2/Classes/Home/View/XXRStatusCell.m
+++ weibo2/weibo2/Classes/Home/View/XXRStatusCell.m
@@ -59,6 +59,7 @@
     
     /** 0.设置cell选中时的背景 */
     self.selectedBackgroundView = [[UIView alloc] init];
+    self.backgroundColor = [UIColor clearColor];
     
     /** 1.顶部的view */
     XXRStatusTopView *topView = [[XXRStatusTopView alloc] init];
