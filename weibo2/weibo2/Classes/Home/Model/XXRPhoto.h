17559e7cf12ca4d8e4d69a2961bd07152ff70e62
Guochao Ren <314017427@qq.com>
2015-10-09 14:52:08 +0800

    添加XXRPhoto模型类

---
+++ weibo2/weibo2/Classes/Home/Model/XXRPhoto.h
@@ -0,0 +1,16 @@
+//
+//  XXRPhoto.h
+//  weibo2
+//
+//  Created by rgc on 15/10/7.
+//  Copyright © 2015年 rgc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XXRPhoto : NSObject
+
+/** 缩略图 */
+@property (nonatomic, strong) NSString *thumbnail_pic;
+
+@end
---
+++ weibo2/weibo2/Classes/Home/Model/XXRPhoto.m
@@ -0,0 +1,13 @@
+//
+//  XXRPhoto.m
+//  weibo2
+//
+//  Created by rgc on 15/10/7.
+//  Copyright © 2015年 rgc. All rights reserved.
+//
+
+#import "XXRPhoto.h"
+
+@implementation XXRPhoto
+
+@end
